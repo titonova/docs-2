d69a4ccf4aee9da14371801eb01ccfd64394024e
PH7-Jack <pedrolivertwd@gmail.com>
2021-11-28 23:10:50 -0300

    chore: install sentry log report

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,35 +7,22 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var string[]
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var string[]
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (Throwable $exception) {
+            if (app()->bound('sentry') && $this->shouldReport($exception)) {
+                app('sentry')->captureException($exception);
+            }
         });
     }
 }
