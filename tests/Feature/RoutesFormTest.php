ec963dc90947c8ce060e0846580bcd423f1bd83b
Daniel Ang <daniel@sysanalyst.eu>
2021-11-30 11:42:09 +0100

    fix: remove duplicated tests

--- tests/Feature/RoutesFormTest.php
+++ tests/Feature/RoutesFormTest.php
@@ -17,20 +17,3 @@ test('Routes: "Form"', function ($route) {
     yield 'docs.datetime-picker';
     yield 'docs.time-picker';
 });
-test('"Form" Routes', function ($route) {
-    $this->get(route($route))
-        ->assertOK();
-})->with(function () {
-    yield 'docs.inputs';
-    yield 'docs.errors';
-    yield 'docs.textarea';
-    yield 'docs.maskable-inputs';
-    yield 'docs.phone-input';
-    yield 'docs.currency-input';
-    yield 'docs.toggle';
-    yield 'docs.checkbox';
-    yield 'docs.radio';
-    yield 'docs.buttons';
-    yield 'docs.datetime-picker';
-    yield 'docs.time-picker';
-});
