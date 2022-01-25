81bc8f2acc0759e208328c80b543f3da199e54d6
Daniel Ang <daniel@sysanalyst.eu>
2022-01-25 18:51:28 +0100

    remove space

--- resources/views/components/green.blade.php
+++ resources/views/components/green.blade.php
@@ -1,2 +1,1 @@
-<span class="px-2 py-2 font-mono text-sm font-semibold text-gray-900 bg-teal-100 rounded-md">{{ $slot }}
-</span>
+<span class="px-2 py-2 font-mono text-sm font-semibold text-gray-900 bg-teal-100 rounded-md">{{ $slot }}</span>
