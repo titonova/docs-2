7ab14411add2b47abb5efbdc578dd9d0377cbbfe
Daniel Ang <daniel@sysanalyst.eu>
2022-01-25 20:37:42 +0100

    [Feat] x-tip component

---
+++ resources/views/components/tip.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center p-4 border rounded-lg gap-x-3 dark:border-0 shadow-soft bg-blue-50 dark:bg-secondary-700">
+  <x-icon name="information-circle" class="flex-shrink-0 w-5 h-5 text-blue-400" />
+  <p class="text-sm text-blue-700 dark:text-blue-400">
+    {{ $slot }}
+  </p>
+</div>
