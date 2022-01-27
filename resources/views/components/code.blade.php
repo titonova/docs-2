ef0a9274d53dd10c3829d84a39c06b3f5a3d5969
Daniel Ang <daniel@sysanalyst.eu>
2022-01-27 14:28:13 +0100

    Refactor x-code to options, remove x-code no-copy

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -1,10 +1,13 @@
-@props(['language', 'code' => null, 'contents' => null])
+@props(['language', 'code' => null, 'contents' => null, 'copy' => true, 'lineNumbers' => true])
 
-<pre>
+<pre @unless($copy === true) data-nocopy="true" @endunless>
 <x-torchlight-code
     {{ $attributes}}
     :contents="$contents"
     :language='$language'>
+@unless($lineNumbers === true)
+// torchlight! {"lineNumbers": false}
+@endunless
 @unless ($contents)
 {!! $code ?? $slot !!}
 @endunless
