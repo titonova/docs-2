724dd5e8bb86f57b1cfbc2ac00e0718ac95eda22
PH7-Jack <pedrolivertwd@gmail.com>
2022-01-31 01:21:01 -0300

    feat: add new buttons examples

--- app/Http/Livewire/Documentation/Buttons.php
+++ app/Http/Livewire/Documentation/Buttons.php
@@ -7,12 +7,8 @@ use Livewire\Component;
 
 class Buttons extends Component
 {
-    public string $simpleButtonExample = <<<HTML
-    <x-button label="No Color" />
-    HTML;
-
-    public string $buttonColorsExample = <<<HTML
-    <x-button label="No Color" />
+    public string $defaultColorsCode = <<<HTML
+    <x-button label="Default" />
     <x-button primary label="Primary" />
     <x-button secondary label="Secondary" />
     <x-button positive label="Positive" />
@@ -20,10 +16,33 @@ class Buttons extends Component
     <x-button warning label="Warning" />
     <x-button info label="Info" />
     <x-button dark label="Dark" />
+    <x-button white label="White" />
+    <x-button black label="Black" />
+    <x-button slate label="Slate" />
+    <x-button gray label="Gray" />
+    <x-button zinc label="Zinc" />
+    <x-button neutral label="Neutral" />
+    <x-button stone label="Stone" />
+    <x-button red label="Red" />
+    <x-button orange label="Orange" />
+    <x-button amber label="Amber" />
+    <x-button lime label="Lime" />
+    <x-button green label="Green" />
+    <x-button emerald label="Emerald" />
+    <x-button teal label="Teal" />
+    <x-button cyan label="Cyan" />
+    <x-button sky label="Sky" />
+    <x-button blue label="Blue" />
+    <x-button indigo label="Indigo" />
+    <x-button violet label="Violet" />
+    <x-button purple label="Purple" />
+    <x-button fuchsia label="Fuchsia" />
+    <x-button pink label="Pink" />
+    <x-button rose label="Rose" />
     HTML;
 
-    public string $outlineButtonsExample = <<<HTML
-    <x-button outline label="No Color" />
+    public string $outlineColorsCode = <<<HTML
+    <x-button outline label="Default" />
     <x-button outline primary label="Primary" />
     <x-button outline secondary label="Secondary" />
     <x-button outline positive label="Positive" />
@@ -31,9 +50,66 @@ class Buttons extends Component
     <x-button outline warning label="Warning" />
     <x-button outline info label="Info" />
     <x-button outline dark label="Dark" />
+    <x-button outline white label="White" />
+    <x-button outline black label="Black" />
+    <x-button outline slate label="Slate" />
+    <x-button outline gray label="Gray" />
+    <x-button outline zinc label="Zinc" />
+    <x-button outline neutral label="Neutral" />
+    <x-button outline stone label="Stone" />
+    <x-button outline red label="Red" />
+    <x-button outline orange label="Orange" />
+    <x-button outline amber label="Amber" />
+    <x-button outline lime label="Lime" />
+    <x-button outline green label="Green" />
+    <x-button outline emerald label="Emerald" />
+    <x-button outline teal label="Teal" />
+    <x-button outline cyan label="Cyan" />
+    <x-button outline sky label="Sky" />
+    <x-button outline blue label="Blue" />
+    <x-button outline indigo label="Indigo" />
+    <x-button outline violet label="Violet" />
+    <x-button outline purple label="Purple" />
+    <x-button outline fuchsia label="Fuchsia" />
+    <x-button outline pink label="Pink" />
+    <x-button outline rose label="Rose" />
     HTML;
 
-    public string $roundedButtonsExample = <<<HTML
+    public string $flatColorsCode = <<<HTML
+    <x-button flat label="Default" />
+    <x-button flat primary label="Primary" />
+    <x-button flat secondary label="Secondary" />
+    <x-button flat positive label="Positive" />
+    <x-button flat negative label="Negative" />
+    <x-button flat warning label="Warning" />
+    <x-button flat info label="Info" />
+    <x-button flat dark label="Dark" />
+    <x-button flat white label="White" />
+    <x-button flat black label="Black" />
+    <x-button flat slate label="Slate" />
+    <x-button flat gray label="Gray" />
+    <x-button flat zinc label="Zinc" />
+    <x-button flat neutral label="Neutral" />
+    <x-button flat stone label="Stone" />
+    <x-button flat red label="Red" />
+    <x-button flat orange label="Orange" />
+    <x-button flat amber label="Amber" />
+    <x-button flat lime label="Lime" />
+    <x-button flat green label="Green" />
+    <x-button flat emerald label="Emerald" />
+    <x-button flat teal label="Teal" />
+    <x-button flat cyan label="Cyan" />
+    <x-button flat sky label="Sky" />
+    <x-button flat blue label="Blue" />
+    <x-button flat indigo label="Indigo" />
+    <x-button flat violet label="Violet" />
+    <x-button flat purple label="Purple" />
+    <x-button flat fuchsia label="Fuchsia" />
+    <x-button flat pink label="Pink" />
+    <x-button flat rose label="Rose" />
+    HTML;
+
+    public string $roundedButtonsCode = <<<HTML
     <x-button rounded label="No Color" />
     <x-button rounded primary label="Primary" />
     <x-button rounded secondary label="Secondary" />
@@ -44,7 +120,7 @@ class Buttons extends Component
     <x-button rounded dark label="Dark" />
     HTML;
 
-    public string $squaredButtonsExample = <<<HTML
+    public string $squaredButtonsCode = <<<HTML
     <x-button squared label="No Color" />
     <x-button squared primary label="Primary" />
     <x-button squared secondary label="Secondary" />
@@ -55,40 +131,33 @@ class Buttons extends Component
     <x-button squared dark label="Dark" />
     HTML;
 
-    public string $flatButtonsExample = <<<HTML
-    <x-button flat label="No Color" />
-    <x-button flat primary label="Primary" />
-    <x-button flat secondary label="Secondary" />
-    <x-button flat positive label="Positive" />
-    <x-button flat negative label="Negative" />
-    <x-button flat warning label="Warning" />
-    <x-button flat info label="Info" />
-    <x-button flat dark label="Dark" />
+    public string $circleButtonsCode = <<<HTML
+    <x-button.circle icon="home" />
+    <x-button.circle primary icon="pencil" />
+    <x-button.circle secondary icon="clipboard-list"  />
+    <x-button.circle positive icon="check" />
+    <x-button.circle negative icon="x" />
+    <x-button.circle warning icon="exclamation" />
+    <x-button.circle info icon="information-circle" />
+    <x-button.circle dark icon="ban" />
+    <x-button.circle secondary label="A"  />
+    <x-button.circle positive label="B" />
+    <x-button.circle negative label="C" />
+    <x-button.circle primary label="+" />
     HTML;
 
-    public string $buttonIconsExample = <<<HTML
+    public string $buttonIconsCode = <<<HTML
     <x-button icon="home" label="No Color" />
     <x-button icon="pencil" primary label="Primary" />
     <x-button icon="clipboard-list" secondary label="Secondary" />
     <x-button icon="check" positive label="Positive" />
     <x-button icon="x" negative label="Negative" />
     <x-button icon="exclamation" warning label="Warning" />
-    <x-button icon="information-circle" info label="Info" />
-    <x-button icon="ban" dark label="Dark" />
+    <x-button right-icon="information-circle" info label="Info" />
+    <x-button right-icon="ban" dark label="Dark" />
     HTML;
 
-    public string $onlyIconsExample = <<<HTML
-    <x-button icon="home" />
-    <x-button icon="pencil" primary />
-    <x-button icon="clipboard-list" secondary />
-    <x-button icon="check" positive />
-    <x-button icon="x" negative />
-    <x-button icon="exclamation" warning />
-    <x-button right-icon="information-circle" info />
-    <x-button right-icon="ban" dark />
-    HTML;
-
-    public string $buttonSizesExample = <<<HTML
+    public string $buttonSizesCode = <<<HTML
     <x-button xs icon="pencil" primary label="Extra Small Button" />
     <x-button sm icon="users" dark label="Small Button" />
     <x-button icon="clipboard-list" secondary label="Normal" />
@@ -96,7 +165,8 @@ class Buttons extends Component
     <x-button lg icon="x" negative label="Large Button" />
     HTML;
 
-    public string $loadingSpinnerExample = <<<HTML
+    public string $loadingSpinnerCode = <<<HTML
+    <x-button.circle wire:click="save" teal icon="check" spinner />
     <x-button wire:click="save" spinner primary label="Save without target" />
     <x-button wire:click="save" spinner="save" primary label="Save with target" />
     <x-button wire:click="save" spinner="save" loading-delay="short" primary label="Save with target + delay indicator" />
--- resources/views/livewire/documentation/buttons.blade.php
+++ resources/views/livewire/documentation/buttons.blade.php
@@ -2,15 +2,14 @@
     <x-slot name="summary">
         <x-summary>
             <x-summary.header href="#buttons" label="Buttons">
-                <x-summary.item href="#simple-button" label="Simple Button" />
-                <x-summary.item href="#button-colors" label="Button Colors" />
+                <x-summary.item href="#default-colors"  label="Default Colors" />
+                <x-summary.item href="#outline-colors"  label="Outline Colors" />
+                <x-summary.item href="#flat-colors"     label="Flat Colors" />
                 <x-summary.item href="#rounded-buttons" label="Rounded Buttons" />
                 <x-summary.item href="#squared-buttons" label="Squared Buttons" />
-                <x-summary.item href="#outline-buttons" label="Outline Buttons" />
-                <x-summary.item href="#flat-buttons" label="Flat Buttons" />
-                <x-summary.item href="#button-icons" label="Button Icons" />
-                <x-summary.item href="#only-icons" label="Only Icons" />
-                <x-summary.item href="#button-sizes" label="Button Sizes" />
+                <x-summary.item href="#circle-buttons"  label="Circle Buttons" />
+                <x-summary.item href="#button-icons"    label="Button With Icons" />
+                <x-summary.item href="#button-sizes"    label="Button Sizes" />
                 <x-summary.item href="#loading-spinner" label="Loading Spinner" />
             </x-summary.header>
 
@@ -20,6 +19,7 @@
 
     <div id="buttons">
         <x-section.title title="Buttons" />
+
         <div class="mt-5 prose xl:mb-8 text-gray-500">
             <p>
                 The button component has multiples styles and options to customize.
@@ -29,24 +29,13 @@
     </div>
 
     <x-code-preview
-        title="Simple Button"
-        href="#simple-button"
-        id="simple-button"
+        title="Default Colors"
+        href="#default-colors"
+        id="default-colors"
         language="html"
-        :code="$simpleButtonExample">
-        <div class="flex flex-wrap gap-4">
-            <x-button label="No Color" />
-        </div>
-    </x-code-preview>
-
-    <x-code-preview
-        title="Button Colors"
-        href="#button-colors"
-        id="button-colors"
-        language="html"
-        :code="$buttonColorsExample">
-        <div class="flex flex-wrap justify-left gap-4">
-            <x-button label="No Color" />
+        :code="$defaultColorsCode">
+        <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 gap-4">
+            <x-button label="Default" />
             <x-button primary label="Primary" />
             <x-button secondary label="Secondary" />
             <x-button positive label="Positive" />
@@ -54,6 +43,111 @@
             <x-button warning label="Warning" />
             <x-button info label="Info" />
             <x-button dark label="Dark" />
+            <x-button white label="White" />
+            <x-button black label="Black" />
+            <x-button slate label="Slate" />
+            <x-button gray label="Gray" />
+            <x-button zinc label="Zinc" />
+            <x-button neutral label="Neutral" />
+            <x-button stone label="Stone" />
+            <x-button red label="Red" />
+            <x-button orange label="Orange" />
+            <x-button amber label="Amber" />
+            <x-button lime label="Lime" />
+            <x-button green label="Green" />
+            <x-button emerald label="Emerald" />
+            <x-button teal label="Teal" />
+            <x-button cyan label="Cyan" />
+            <x-button sky label="Sky" />
+            <x-button blue label="Blue" />
+            <x-button indigo label="Indigo" />
+            <x-button violet label="Violet" />
+            <x-button purple label="Purple" />
+            <x-button fuchsia label="Fuchsia" />
+            <x-button pink label="Pink" />
+            <x-button rose label="Rose" />
+        </div>
+    </x-code-preview>
+
+    <x-code-preview
+        title="Outline Colors"
+        href="#outline-colors"
+        id="outline-colors"
+        language="html"
+        :code="$outlineColorsCode">
+        <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 gap-4">
+            <x-button outline label="Default" />
+            <x-button outline primary label="Primary" />
+            <x-button outline secondary label="Secondary" />
+            <x-button outline positive label="Positive" />
+            <x-button outline negative label="Negative" />
+            <x-button outline warning label="Warning" />
+            <x-button outline info label="Info" />
+            <x-button outline dark label="Dark" />
+            <x-button outline white label="White" />
+            <x-button outline black label="Black" />
+            <x-button outline slate label="Slate" />
+            <x-button outline gray label="Gray" />
+            <x-button outline zinc label="Zinc" />
+            <x-button outline neutral label="Neutral" />
+            <x-button outline stone label="Stone" />
+            <x-button outline red label="Red" />
+            <x-button outline orange label="Orange" />
+            <x-button outline amber label="Amber" />
+            <x-button outline lime label="Lime" />
+            <x-button outline green label="Green" />
+            <x-button outline emerald label="Emerald" />
+            <x-button outline teal label="Teal" />
+            <x-button outline cyan label="Cyan" />
+            <x-button outline sky label="Sky" />
+            <x-button outline blue label="Blue" />
+            <x-button outline indigo label="Indigo" />
+            <x-button outline violet label="Violet" />
+            <x-button outline purple label="Purple" />
+            <x-button outline fuchsia label="Fuchsia" />
+            <x-button outline pink label="Pink" />
+            <x-button outline rose label="Rose" />
+        </div>
+    </x-code-preview>
+
+    <x-code-preview
+        title="Flat Colors"
+        href="#flat-colors"
+        id="flat-colors"
+        language="html"
+        :code="$flatColorsCode">
+        <div class="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 gap-4">
+            <x-button flat label="Default" />
+            <x-button flat primary label="Primary" />
+            <x-button flat secondary label="Secondary" />
+            <x-button flat positive label="Positive" />
+            <x-button flat negative label="Negative" />
+            <x-button flat warning label="Warning" />
+            <x-button flat info label="Info" />
+            <x-button flat dark label="Dark" />
+            <x-button flat white label="White" />
+            <x-button flat black label="Black" />
+            <x-button flat slate label="Slate" />
+            <x-button flat gray label="Gray" />
+            <x-button flat zinc label="Zinc" />
+            <x-button flat neutral label="Neutral" />
+            <x-button flat stone label="Stone" />
+            <x-button flat red label="Red" />
+            <x-button flat orange label="Orange" />
+            <x-button flat amber label="Amber" />
+            <x-button flat lime label="Lime" />
+            <x-button flat green label="Green" />
+            <x-button flat emerald label="Emerald" />
+            <x-button flat teal label="Teal" />
+            <x-button flat cyan label="Cyan" />
+            <x-button flat sky label="Sky" />
+            <x-button flat blue label="Blue" />
+            <x-button flat indigo label="Indigo" />
+            <x-button flat violet label="Violet" />
+            <x-button flat purple label="Purple" />
+            <x-button flat fuchsia label="Fuchsia" />
+            <x-button flat pink label="Pink" />
+            <x-button flat rose label="Rose" />
         </div>
     </x-code-preview>
 
@@ -62,7 +156,7 @@
         href="#rounded-buttons"
         id="rounded-buttons"
         language="html"
-        :code="$roundedButtonsExample">
+        :code="$roundedButtonsCode">
         <div class="flex flex-wrap justify-left gap-4">
             <x-button rounded label="No Color" />
             <x-button rounded primary label="Primary" />
@@ -80,7 +174,7 @@
         href="#squared-buttons"
         id="squared-buttons"
         language="html"
-        :code="$squaredButtonsExample">
+        :code="$squaredButtonsCode">
         <div class="flex flex-wrap justify-left gap-4">
             <x-button squared label="No Color" />
             <x-button squared primary label="Primary" />
@@ -94,47 +188,33 @@
     </x-code-preview>
 
     <x-code-preview
-        title="Outline Buttons"
-        href="#outline-buttons"
-        id="outline-buttons"
+        title="Circle Buttons"
+        href="#circle-buttons"
+        id="circle-buttons"
         language="html"
-        :code="$outlineButtonsExample">
-        <div class="flex flex-wrap justify-left gap-4">
-            <x-button outline label="No Color" />
-            <x-button outline primary label="Primary" />
-            <x-button outline secondary label="Secondary" />
-            <x-button outline positive label="Positive" />
-            <x-button outline negative label="Negative" />
-            <x-button outline warning label="Warning" />
-            <x-button outline info label="Info" />
-            <x-button outline dark label="Dark" />
+        :code="$circleButtonsCode">
+        <div class="flex flex-wrap gap-x-2">
+            <x-button.circle icon="home" />
+            <x-button.circle primary icon="pencil" />
+            <x-button.circle secondary icon="clipboard-list"  />
+            <x-button.circle positive icon="check" />
+            <x-button.circle negative icon="x" />
+            <x-button.circle warning icon="exclamation" />
+            <x-button.circle info icon="information-circle" />
+            <x-button.circle dark icon="ban" />
+            <x-button.circle secondary label="A"  />
+            <x-button.circle positive label="B" />
+            <x-button.circle negative label="C" />
+            <x-button.circle primary label="+" />
         </div>
     </x-code-preview>
 
     <x-code-preview
-        title="Flat Buttons"
-        href="#flat-buttons"
-        id="flat-buttons"
-        language="html"
-        :code="$flatButtonsExample">
-        <div class="flex flex-wrap justify-left gap-4">
-            <x-button flat label="No Color" />
-            <x-button flat primary label="Primary" />
-            <x-button flat secondary label="Secondary" />
-            <x-button flat positive label="Positive" />
-            <x-button flat negative label="Negative" />
-            <x-button flat warning label="Warning" />
-            <x-button flat info label="Info" />
-            <x-button flat dark label="Dark" />
-        </div>
-    </x-code-preview>
-
-    <x-code-preview
-        title="Button Icons"
+        title="Button With Icons"
         href="#button-icons"
         id="button-icons"
         language="html"
-        :code="$buttonIconsExample">
+        :code="$buttonIconsCode">
         <div class="flex flex-wrap justify-left gap-4">
             <x-button icon="home" label="No Color" />
             <x-button icon="pencil" primary label="Primary" />
@@ -148,45 +228,19 @@
     </x-code-preview>
 
     <x-code-preview
-        title="Only Icons"
-        href="#only-icons"
-        id="only-icons"
-        language="html"
-        :code="$onlyIconsExample">
-        <div class="flex flex-wrap justify-left gap-4">
-            <x-button icon="home" />
-            <x-button icon="pencil" primary />
-            <x-button icon="clipboard-list" secondary />
-            <x-button icon="check" positive />
-            <x-button icon="x" negative />
-            <x-button icon="exclamation" rounded warning />
-            <x-button right-icon="information-circle" info />
-            <x-button right-icon="ban" dark />
-        </div>
-    </x-code-preview>
-
-    <x-code-preview
         title="Button Sizes"
         href="#button-sizes"
         id="button-sizes"
         language="html"
-        :code="$buttonSizesExample">
-        <div class="flex flex-wrap justify-left gap-4">
-            <div>
-                <x-button xs icon="pencil" primary label="Extra Small Button" />
-            </div>
-            <div>
-                <x-button sm icon="users" dark label="Small Button" />
-            </div>
-            <div>
-                <x-button icon="clipboard-list" secondary label="Normal" />
-            </div>
-            <div>
-                <x-button md icon="check" positive label="Medium Button" />
-            </div>
-            <div>
-                <x-button lg icon="x" negative label="Large Button" />
-            </div>
+        :code="$buttonSizesCode">
+        <div class="space-x-2 space-y-2">
+            <x-button 2xs secondary label="2xs size" />
+            <x-button xs  primary   label="xs size" />
+            <x-button sm  positive  label="sm size" />
+            <x-button     negative  label="default size" />
+            <x-button md  warning   label="md size" />
+            <x-button lg  stone     label="lg size" />
+            <x-button xl  dark      label="xl size" />
         </div>
     </x-code-preview>
 
@@ -205,8 +259,10 @@
         href="#loading-spinner"
         id="loading-spinner"
         language="html"
-        :code="$loadingSpinnerExample">
-        <div class="flex gap-4 items-center">
+        :code="$loadingSpinnerCode">
+        <div class="flex flex-wrap gap-4 items-center">
+            <x-button.circle wire:click="$refresh" teal icon="check" spinner />
+
             <x-button wire:click="$refresh" spinner primary label="Save without target" />
 
             <x-button wire:click="sleeping" spinner="sleeping" primary label="Save with target" />
@@ -215,35 +271,38 @@
         </div>
     </x-code-preview>
 
+    <div class="flex items-center gap-x-3 rounded-lg border dark:border-0 shadow-soft bg-yellow-50 dark:bg-secondary-700 p-4">
+        <x-icon name="exclamation" class="h-5 w-5 shrink-0 text-yellow-400" />
+
+        <p class="text-sm text-yellow-700 dark:text-yellow-400">
+            The attributes [squared, rounded, right-icon] are not supported in <b>button.circle</b> component
+        </p>
+    </div>
+
     <div id="button-options">
         <x-section.title href="#button-options" title="Button Options" />
         <x-options-table class="mt-2 mb-6 w-full">
-            <x-option-table-row prop="xs"        required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="md"        required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="lg"        required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="primary"   required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="secondary" required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="positive"  required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="negative"  required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="warning"   required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="info"      required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="dark"      required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="rounded"   required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="squared"   required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="bordered"  required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="flat"      required="false" default="false" type="boolean" available="true|false" />
-            <x-option-table-row prop="color"     required="false" default="none" type="string"   available="colors" />
-            <x-option-table-row prop="size"      required="false" default="sm"   type="string"   available="xs|sm|md|lg" />
-            <x-option-table-row prop="label"     required="false" default="none" type="string"   available="" />
+            <x-option-table-row prop="xs"        required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="md"        required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="lg"        required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="primary"   required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="secondary" required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="positive"  required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="negative"  required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="warning"   required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="info"      required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="dark"      required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="rounded"   required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="squared"   required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="bordered"  required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="flat"      required="false" default="false" type="boolean" available="boolean" />
+            <x-option-table-row prop="color"     required="false" default="none" type="string"   available="default colors + all tailwind colors" />
+            <x-option-table-row prop="size"      required="false" default="sm"   type="string"   available="2xs|xs|sm|default|md|lg|xl" />
+            <x-option-table-row prop="label"     required="false" default="none" type="string"   available="*" />
             <x-option-table-row prop="icon"      required="false" default="none" type="string"   available="all heroicons" />
             <x-option-table-row prop="rightIcon" required="false" default="none" type="string"   available="all heroicons" />
-            <x-option-table-row prop="spinner" required="false" default="none" type="boolean|string" available="--" />
+            <x-option-table-row prop="spinner"   required="false" default="none" type="boolean|string" available="*" />
             <x-option-table-row prop="loadingDelay" required="false" default="none" type="string" available="shortest|shorter|short|long|longer|longest" />
         </x-options-table>
-
-        <label class="mb-1 text-gray-700 font-semibold">Colors</label>
-        <x-code language="js" :code="<<<EOT
-        primary|secondary|positive|negative|warning|info|dark
-        EOT" />
     </div>
 </div>
