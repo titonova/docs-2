28e6cfa2ea944b71b29c2cdc3a18586019ec907e
PH7-Jack <pedrolivertwd@gmail.com>
2022-02-14 23:16:07 -0300

    tests: add x-code test

---
+++ tests/Unit/Components/CodeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Components;
+
+use App\Http\Livewire\Documentation\GetStarted;
+use Illuminate\Support\Facades\Route;
+use Tests\TestCase;
+
+class CodeTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->fakeTorchlight();
+    }
+
+    /** @test */
+    public function it_should_make_the_code_blocks()
+    {
+        $this->blade('<x-code language="php">echo "hello world";</x-code>')
+            ->assertSee('<pre >##PRE_TL_COMPONENT##<code __torchlight-block', escape: false)
+            ->assertSee('</code>##POST_TL_COMPONENT##</pre>', escape: false);
+    }
+
+    /** @test */
+    public function it_should_render_the_highlighted_code()
+    {
+        Route::get('test-code', GetStarted::class);
+
+        $this->get('/test-code')
+            ->assertSee('<pre ><code data-theme="material-theme-palenight" data-lang="shell" class="torchlight" style="background-color: #000000;"><div class="highlighted">// torchlight! {"lineNumbers":false}', escape: false)
+            ->assertSee('composer require wireui/wireui</div></code></pre>', escape: false);
+    }
+}
