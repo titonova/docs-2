12090aa435ab37d2fd97917f4d8fa6a04f7d305b
PH7-Jack <pedrolivertwd@gmail.com>
2022-02-14 23:12:49 -0300

    tests: remove info test

--- tests/Unit/Components/Alerts/InfoTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-it('should render the slot content', function () {
-    $view = $this->blade('<x-alerts.info>WireUI is amazing!</x-alerts.info>');
-
-    $view->assertSee('WireUI is amazing!');
-});
