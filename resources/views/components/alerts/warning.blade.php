0e5af06c4b4987276cadf4c8ef3a3262d0668f72
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-03 04:15:41 -0300

    fix long text on small devices

--- resources/views/components/alerts/warning.blade.php
+++ resources/views/components/alerts/warning.blade.php
@@ -6,7 +6,7 @@
 <div class="flex items-start gap-x-3 rounded-lg border dark:border-0 shadow-soft bg-yellow-50 dark:bg-secondary-700 p-4">
     <x-icon name="exclamation" class="h-5 w-5 shrink-0 text-yellow-400" />
 
-    <div>
+    <div class="break-all">
         <h3 class="text-sm font-medium text-yellow-800 dark:text-yellow-400">
             {{ $title }}
         </h3>
