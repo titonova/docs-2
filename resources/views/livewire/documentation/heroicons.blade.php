23b2bdeed104aacc7668972e4e04828c0ce52b00
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-06 00:52:42 -0300

    add modifiers style doc for icons

--- resources/views/livewire/documentation/heroicons.blade.php
+++ resources/views/livewire/documentation/heroicons.blade.php
@@ -15,7 +15,7 @@
 
         <x-code language="html" :contents='<<<EOT
         <x-icon name="home" class="w-5 h-5" />
-        <x-icon name="home" style="solid" class="w-5 h-5" />
+        <x-icon name="home" class="w-5 h-5" solid />
         EOT' />
 
         <p>You can publish the icon configuration and define what will be the default style</p>
@@ -29,7 +29,9 @@
     <x-section.title id="options" href="options" title="Options" />
 
     <x-options-table class="mt-10">
-        <x-option-table-row prop="name"  required="true"  default="none"    type="string" available="all heroicons" />
-        <x-option-table-row prop="style" required="false" default="outline" type="string" available="outline|solid" />
+        <x-option-table-row prop="name"    required="true"  default="none"    type="string" available="all heroicons" />
+        <x-option-table-row prop="style"   required="false" default="outline" type="string" available="outline|solid" />
+        <x-option-table-row prop="solid"   required="false" default="false"   type="boolean" available="true|false" />
+        <x-option-table-row prop="outline" required="false" default="false"   type="boolean" available="true|false" />
     </x-options-table>
 </div>
